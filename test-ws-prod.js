a301a38708e96a4644132213e27914b8f74c8f1d
HridayBardia <hridaybardia07@gmail.com>
2026-06-09 20:13:03 +0530

    sync update

--- test-ws-prod.js
+++ test-ws-prod.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket("ws://localhost:8080/ws");
+const ws = new WebSocket("wss://wavesync-backend-0j3v.onrender.com/ws");
 ws.onopen = () => {
   ws.send(JSON.stringify({ type: "JOIN_ROOM", roomCode: "TEST", displayName: "Tester" }));
   ws.send(JSON.stringify({ type: "NTP_REQUEST", t0: Date.now() }));
